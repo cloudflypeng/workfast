28fcb56c11616f2c755070d4b1452270606cce89
pengyunfei <pengyunfei@360.cn>
2022-03-17 12:13:44 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './header';
+import Snip from './maincomponent/snip'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Snip />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/header/index.js
@@ -0,0 +1,10 @@
+const Header = () =>{
+  return(
+    <headerc className="w-screen fixed top-0 left-0 block flex items-center justify-center h-20">
+      <div className="block w-40">首页</div>
+      <div className="w-40">关于我</div>
+    </headerc>
+  )
+}
+
+export default Header
